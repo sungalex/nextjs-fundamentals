50c98c8d714364b50e7d13bb248d28d627e24eba
sungalex <cloudallera@gmail.com>
2022-05-30 13:04:29 +0900

    Fetching Data(TMDB), Redriects and Rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+      // {
+      //   source: "/api/movies/image/:id*",
+      //   destination: "https://image.tmdb.org/t/p/w500/:id*",
+      //   permanent: false,
+      // },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies/popular",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/image/:id*",
+        destination: "https://image.tmdb.org/t/p/w500/:id*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
