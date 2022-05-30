abe8ba421e7406ff17ae5bda11e13ffc590288d3
sungalex <cloudallera@gmail.com>
2022-05-30 10:13:47 +0900

    Patterns

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1>Home</h1>
     </div>
   );
