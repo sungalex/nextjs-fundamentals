08bba5ac7685d71e8c0ce245755d9ba88be34378
sungalex <cloudallera@gmail.com>
2022-05-31 01:22:51 +0900

    404 Pages

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <Seo title="Error" />
+      <h4>PageNotFound: What are you doing here?</h4>
+    </div>
+  );
+}
